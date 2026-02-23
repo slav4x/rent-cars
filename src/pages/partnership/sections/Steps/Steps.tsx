1dbbe8a7864c7cc64f26a0f901df630b8ea7c1cf
Slava Khripushin <slava.cpa@yandex.ru>
2026-02-23 16:06:40 +0700

    fix: Steps counter + image

--- src/pages/partnership/sections/Steps/Steps.tsx
+++ src/pages/partnership/sections/Steps/Steps.tsx
@@ -24,38 +24,80 @@ export default function Steps() {
 						onItemClick={() => {}}
 					>
 						<div className={styles.card}>
-							<Image src="img/steps-01.jpg" alt="" width={420} height={490} unoptimized={true} />
+							<Image
+								src="img/steps-01.jpg"
+								alt=""
+								width={420}
+								height={490}
+								unoptimized={true}
+								className={styles.image}
+							/>
 							<div className={styles.title}>оставляете заявку</div>
 							<div className={styles.text}>Вы оставляете заявку на сайте с указанием Ваших контактных данных</div>
 						</div>
 						<div className={styles.card}>
-							<Image src="img/steps-02.jpg" alt="" width={420} height={490} unoptimized={true} />
+							<Image
+								src="img/steps-02.jpg"
+								alt=""
+								width={420}
+								height={490}
+								unoptimized={true}
+								className={styles.image}
+							/>
 							<div className={styles.title}>уточнение деталей</div>
 							<div className={styles.text}>В ближайшее время Вам перезванивает менеджер для уточнения всех данных</div>
 						</div>
 						<div className={styles.card}>
-							<Image src="img/steps-03.jpg" alt="" width={420} height={490} unoptimized={true} />
+							<Image
+								src="img/steps-03.jpg"
+								alt=""
+								width={420}
+								height={490}
+								unoptimized={true}
+								className={styles.image}
+							/>
 							<div className={styles.title}>составление договора</div>
 							<div className={styles.text}>
 								Наш менеждер подбирает выгодные условия для обеих сторон, после чего присылает Вам для ознакомления
 							</div>
 						</div>
 						<div className={styles.card}>
-							<Image src="img/steps-04.jpg" alt="" width={420} height={490} unoptimized={true} />
+							<Image
+								src="img/steps-04.jpg"
+								alt=""
+								width={420}
+								height={490}
+								unoptimized={true}
+								className={styles.image}
+							/>
 							<div className={styles.title}>проверка aвтомобиля</div>
 							<div className={styles.text}>
 								Выезжаем на назначенное заранее место для осмотра автомобиля для сдачи в аренду
 							</div>
 						</div>
 						<div className={styles.card}>
-							<Image src="img/steps-05.jpg" alt="" width={420} height={490} unoptimized={true} />
+							<Image
+								src="img/steps-05.jpg"
+								alt=""
+								width={420}
+								height={490}
+								unoptimized={true}
+								className={styles.image}
+							/>
 							<div className={styles.title}>заключение договора</div>
 							<div className={styles.text}>
 								После полного одобрения заключаем договор, забираем автомобиль и начинаем работу
 							</div>
 						</div>
 						<div className={styles.card}>
-							<Image src="img/steps-06.jpg" alt="" width={420} height={490} unoptimized={true} />
+							<Image
+								src="img/steps-06.jpg"
+								alt=""
+								width={420}
+								height={490}
+								unoptimized={true}
+								className={styles.image}
+							/>
 							<div className={styles.title}>Выплата по договору</div>
 							<div className={styles.text}>Согласно договору, выплачиваем деньги каждые две недели или месяц</div>
 						</div>
